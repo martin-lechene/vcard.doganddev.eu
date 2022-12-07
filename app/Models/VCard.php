0e1e8bb430525a79c061937c2102ddaab9621707
MARTIN LECHENE <martin@lechene.be>
2022-12-07 13:03:58 +0100

    Update VCard model

--- app/Models/VCard.php
+++ app/Models/VCard.php
@@ -4,120 +4,88 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-//use App\Http\Controllers\VCardController;
-//use JeroenDesloovere\VCard\VCard;+
+use App\Http\Controllers\VCardController;
+//use JeroenDesloovere\VCard\VCard;
 
 
 class VCard extends Model
 {
     use HasFactory;
 
-//    public function __construct()
+    // filaable fields
+    protected $fillable = [
+        'lastname',
+        'firstname',
+        'additional',
+        'prefix',
+        'suffix',
+        'company',
+        'department',
+        'jobtitle',
+        'role',
+        'email',
+        'phone_pref_work',
+        'phone_work',
+        'fax_work',
+        'phone_home',
+        'fax_home',
+        'address_name',
+        'address_extended',
+        'address_street',
+        'address_city',
+        'address_region',
+        'address_zip',
+        'address_country',
+        'website',
+        'birthday',
+        'note',
+        'photo'
+    ];
+    // auto generate line in database for table download
+//    public function downloads()
 //    {
-////        $this->name = 'Martin Lechene';
-////        $this->title = 'Web Developer';
-////        $this->email = 'martin@lechene.be';
-////        $this->phone = '+32 495 77 77 77';
-////        $this->address = 'Rue de la Paix 1, 1000 Bruxelles';
-////        $this->website = 'https://martin.lechene.be';
+//        return $this->hasMany(Download::class);
 //    }
-//
-//    public function addName($name)
-//    {
-//        $this->name = $name;
-//    }
-//
-//    // addCompany()
-//    public function addCompany($company)
-//    {
-//        $this->company = $company;
-//    }
-//
-//    // addJobtitle()
-//    public function addJobtitle($title)
-//    {
-//        $this->title = $title;
-//    }
-//
-//    // addRole()
-//    public function addRole($role)
-//    {
-//        $this->role = $role;
-//    }
-//
-//    // addEmail()
-//    public function addEmail($email)
-//    {
-//        $this->email = $email;
-//    }
-//
-//    // addPhoneNumber()
-//    public function addPhoneNumber($phone)
-//    {
-//        $this->phone = $phone;
-//    }
-//
-//    // addAddress()
-//    public function addAddress($address)
-//    {
-//        $this->address = $address;
-//    }
-//
-//    // addLabel()
-//    public function addLabel($label)
-//    {
-//        $this->label = $label;
-//    }
-//
-//    // addURL()
-//    public function addURL($website)
-//    {
-//        $this->website = $website;
-//    }
-//
-//    // addPhoto()
-//    public function addPhoto($photo)
-//    {
-//        $this->photo = $photo;
-//    }
-//
-//    // download()
-//    public function download()
-//    {
-//        $vcard = "
-//            BEGIN:VCARD
-//            VERSION:3.0
-//            N:{$this->name}
-//            FN:{$this->name}
-//            TITLE:{$this->title}
-//            EMAIL:{$this->email}
-//            TEL:{$this->phone}
-//            ADR:{$this->address}
-//            URL:{$this->website}
-//            END:VCARD
-//        ";
-//
-//        return response($vcard, 200)
-//            ->header('Content-Type', 'text/x-vcard')
-//            ->header('Content-Disposition', 'attachment; filename="martin.lechene.vcf"');
-//    }
-//
-//    // getOutput()
-//    public function getOutput()
-//    {
-//        $vcard = "
-//            BEGIN:VCARD
-//            VERSION:3.0
-//            N:{$this->name}
-//            FN:{$this->name}
-//            TITLE:{$this->title}
-//            EMAIL:{$this->email}
-//            TEL:{$this->phone}
-//            ADR:{$this->address}
-//            URL:{$this->website}
-//            END:VCARD
-//        ";
-//
-//        return $vcard;
-//    }
+
+    // all vcards since mysql
+    public function allVcards()
+    {
+        $vcards = VCard::all();
+        return $vcards;
+    }
+
+
+    // delete vcard
+    public function deleteVCard($id)
+    {
+        // Declaration of App\Models\VCard::delete($id) must be compatible with Illuminate\Database\Eloquent\Model::delete()
+
+        $vcard = VCard::find($id);
+        $vcard->delete();
+
+        return true;
+    }
+
+    // create vcard
+    public function createVcard()
+    {
+        $vcard = new VCard();
+        $vcard->lastname = 'Doe';
+        $vcard->firstname = 'John';
+        $vcard->additional = 'van';
+        $vcard->prefix = 'Mr.';
+        $vcard->suffix = 'III';
+        $vcard->company = 'Acme Inc.';
+        $vcard->department = 'Sales';
+        $vcard->jobtitle = 'Salesman';
+        $vcard->role = 'Manager';
+        $vcard->email = '';
+    }
+
+    public function latest($id)
+    {
+        $vcard = VCard::find($id);
+        return $vcard;
+    }
+
 }
