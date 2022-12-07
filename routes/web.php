52808b0c952cb7fbbaa229acfb23a24af086bad8
MARTIN LECHENE <martin@lechene.be>
2022-12-07 13:06:11 +0100

    Update web

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,16 @@ Route::get('/', function () {
 Route::group(['prefix' => 'vcard'], function () {
     Route::get('/', [VCardController::class, 'index'])->name('vcard.index');
     Route::get('/create', [VCardController::class, 'create'])->name('vcard.create');// GET
-    Route::post('/create', [VCardController::class, 'store'])->name('vcard.create.store');// POST
-//    Route::post('/store', [VCardController::class, 'store'])->name('vcard.store');
+    Route::post('/create', [VCardController::class, 'store'])->name('vcard.create');// POST
+    Route::get('/store', [VCardController::class, 'store'])->name('vcard.store');
+    Route::post('/store', [VCardController::class, 'store'])->name('vcard.store');
     Route::get('/edit/{id}', [VCardController::class, 'edit'])->name('vcard.edit');
     Route::post('/update/{id}', [VCardController::class, 'update'])->name('vcard.update');
-    Route::get('/delete/{id}', [VCardController::class, 'destroy'])->name('vcard.delete');
+    // Delete Supported methods GET,HEAD
+    // destroy Supported methods DELETE
+    Route::delete('/delete/{id}', [VCardController::class, 'destroy'])->name('vcard.destroy');
+    Route::get('/delete/{id}', [VCardController::class, 'delete'])->name('vcard.delete');
+    Route::post('/delete/{id}', [VCardController::class, 'delete'])->name('vcard.delete');
     // downloadVcard
     Route::get('/downloadVcard/{id}', [VCardController::class, 'downloadVcard'])->name('vcard.downloadVcard');
 });
